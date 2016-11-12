1eb8288f1d40267ab5aebdfc4e87883267e5bb33
Trexter <sheridak@purdue.edu>
2016-11-12 11:50:10 -0500

    added montecarlo

---
+++ include/mantis/MonteCarlo.cpp
@@ -0,0 +1,18 @@
+/*
+ * MonteCarlo.cpp
+ *
+ *  Created on: Nov 12, 2016
+ *      Author: kevin
+ */
+
+#include "MonteCarlo.h"
+
+MonteCarlo::MonteCarlo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MonteCarlo::~MonteCarlo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ include/mantis/MonteCarlo.h
@@ -0,0 +1,17 @@
+/*
+ * MonteCarlo.h
+ *
+ *  Created on: Nov 12, 2016
+ *      Author: kevin
+ */
+
+#ifndef MANTIS_INCLUDE_MANTIS_MONTECARLO_H_
+#define MANTIS_INCLUDE_MANTIS_MONTECARLO_H_
+
+class MonteCarlo {
+public:
+	MonteCarlo();
+	virtual ~MonteCarlo();
+};
+
+#endif /* MANTIS_INCLUDE_MANTIS_MONTECARLO_H_ */
