3f5c61a8f4c6250063057b3ea5a168966cf6359e
wherman <wherman@purdue.edu>
2017-02-10 19:45:44 -0500

    Set up computeError.h

--- include/mantis/error/computeError.h
+++ include/mantis/error/computeError.h
@@ -8,7 +8,26 @@
 #ifndef MANTIS_INCLUDE_MANTIS_ERROR_COMPUTEERROR_H_
 #define MANTIS_INCLUDE_MANTIS_ERROR_COMPUTEERROR_H_
 
+struct MantisRequest {
+	struct cam {
+		cv::flann:: KDTreeIndexParams paramIndex_w;
+		cv::flann:: Index kdtree_w;
+		cv::flann:: KDTreeIndexParams paramIndex_r;
+		cv::flann:: Index kdtree_r;
+		cv::flann:: KDTreeIndexParams paramIndex_g;
+		cv::flann:: Index kdtree_g;
+	};
 
+	cam c1;
+	cam c2;
+};
+
+
+
+double computeError(MantisRequest& req)
+{
+	return 0;
+}
 
 
 
