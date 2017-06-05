e685ed4469fef77e1abb03941fc837ab4d38b45c
Trexter <sheridak@purdue.edu>
2017-06-05 18:21:33 -0400

    new method

--- src/mantis_client.cpp
+++ src/mantis_client.cpp
@@ -13,11 +13,23 @@
 
 #define SUPER_DEBUG true
 
-#define QUAD_STRECH_FACTOR 1.3
+#define QUAD_STRECH_FACTOR 1.31
+#define MASK_THICKNESS_FACTOR 0.25
 
-#define WHITE_INIT cv::Vec3b(255, 255, 255)
-#define RED_INIT cv::Vec3b(0, 0, 255)
-#define GREEN_INIT cv::Vec3b(0, 255, 0)
+//HSV mins and maxes
+#define WHITE_MIN cv::Scalar(0, 0, 80)
+
+#define RED_MIN1 cv::Scalar(0, 170, 170)
+#define RED_MIN2 cv::Scalar(165, 170, 170)
+
+#define GREEN_MIN cv::Scalar(70, 70, 50)
+
+#define WHITE_MAX cv::Scalar(360, 20, 100)
+
+#define RED_MAX1 cv::Scalar(10, 255, 255)
+#define RED_MAX2 cv::Scalar(180, 255, 255)
+
+#define GREEN_MAX cv::Scalar(140, 100, 100)
 
 struct Frame;
 void run(Frame* f);
@@ -27,15 +39,31 @@ int CANNY_HYSTERESIS;
 int RATE;
 int POLYGON_EPSILON;
 std::string CAMERA_0_TOPIC;
-cv::Vec3b RED_BGR, GREEN_BGR, WHITE_BGR;
+//cv::Vec3b RED_BGR, GREEN_BGR, WHITE_BGR;
 double COLOR_THRESHOLD;
 double SEARCH_RADIUS_MULTIPLIER;
 
+#ifdef SUPER_DEBUG
+cv::Mat final;
+bool imgReady = false;
+#endif
+/*
 enum Color{
 	RED,
 	GREEN,
 	WHITE,
 	OTHER
+};*/
+
+struct Measurement{
+	cv::Point pos;
+	cv::Scalar color;
+
+	Measurement(cv::Point _pos, cv::Scalar _color)
+	{
+		pos = _pos;
+		color = _color;
+	}
 };
 
 struct Quadrilateral
@@ -43,8 +71,9 @@ struct Quadrilateral
 	cv::Point2f center; // the centroid of this quad
 	std::vector<cv::Point> contour; // the raw detected corners of this quad
 	std::vector<cv::Point> test_points; // test points of the quad for color
+
 	double side_length; // the approximate area of this quad
-	Color color; // the color of this quad
+	//Color color; // the color of this quad
 	bool neighbor; // has this quad been found to be the neighbor of another already
 
 	cv::Point2f computeQuadCenter(std::vector<cv::Point>& contour)
@@ -60,7 +89,7 @@ struct Quadrilateral
 	}
 
 	std::vector<cv::Point> computeTestPoints()
-	{
+																	{
 		std::vector<cv::Point> test;
 		for(auto e : contour)
 		{
@@ -68,7 +97,7 @@ struct Quadrilateral
 			test.push_back(pt);
 		}
 		return test;
-	}
+																	}
 
 	Quadrilateral(std::vector<cv::Point> _contour)
 	{
@@ -81,7 +110,7 @@ struct Quadrilateral
 		this->side_length = sqrt(first_delta.x*first_delta.x + first_delta.y*first_delta.y);
 		ROS_DEBUG_STREAM("side length: " << side_length);
 		this->center = this->computeQuadCenter(_contour);
-		this->color = Color::OTHER;
+		//this->color = Color::OTHER;
 
 		//test points
 		test_points = computeTestPoints();
@@ -102,47 +131,25 @@ struct Frame
 
 Frame frame0;
 
-/*
- * get the color of a pixel by searching around it for colors in the THRESH
- */
-Color getCornerColor(Frame* f, cv::Point& pos)
+bool checkPixel(cv::Mat sample, cv::Scalar min, cv::Scalar max)
 {
-	Color theColor = Color::OTHER;
-
-	// get the search region
-	cv::Rect roi(pos.x, pos.y, 1 , 1);
-	cv::Mat searchMat = f->img(roi);
-
-	return theColor;
-}
-
-/*
- * get the color of a quadrilateral
- * RED = quad near the RED line
- * GREEN = quad near the GREEN Line
- * WHITE = quad in the center of the grid
- * OTHER = outlier
- */
-Color getQuadColor(Frame* f, Quadrilateral quad)
-{
-	Color finalColor = Color::WHITE;
-
-	for(std::vector<cv::Point>::iterator it = quad.contour.begin(); it != quad.contour.begin() + 4; it++)
+	//ROS_DEBUG_STREAM(min.val[0] << " " << min.val[1]);
+	//ROS_DEBUG_STREAM((int)sample.data[0] << " " << (int)sample.data[1]);
+	if(!(sample.data[0] >= min.val[0] && sample.data[0] <= max.val[0]))
 	{
-		//cv::Point test = (cv::Point2f((*it)) - quad.center) * QUAD_STRECH_FACTOR + quad.center;
-		Color thisCorner = getCornerColor(f, (*it));
-		if(thisCorner == Color::OTHER)
-		{
-			return Color::OTHER; // this is an invalid corner
-		}
-		else if(thisCorner == Color::GREEN || thisCorner == Color::RED)
-		{
-			return thisCorner;
-		}
+		return false;
 	}
-
-	return finalColor;
+	if(!(sample.data[1] >= min.val[1] && sample.data[1] <= max.val[1]))
+	{
+		return false;
+	}
+	if(!(sample.data[2] >= min.val[2] && sample.data[2] <= max.val[2]))
+	{
+		return false;
+	}
+	return true;
 }
+
 
 void convert2Binary(cv::Mat& img){
 
@@ -158,6 +165,7 @@ void convert2Binary(cv::Mat& img){
 	}
 
 }
+
 
 int removeDuplicateQuads(std::vector<Quadrilateral>& quads)
 {
@@ -204,6 +212,22 @@ int removeDuplicateQuads(std::vector<Quadrilateral>& quads)
 
 }
 
+cv::Mat createGridMask(std::vector<Quadrilateral> quads, int rows, int cols)
+{
+	cv::Mat mask = cv::Mat::zeros(rows, cols, CV_8U);
+
+	for(auto e : quads)
+	{
+		int thickness = e.side_length * MASK_THICKNESS_FACTOR; // TODO make thickness a func of sidelength
+		ROS_ASSERT(e.test_points.size() == 4);
+		std::vector<std::vector<cv::Point> > cont;
+		cont.push_back(e.test_points);
+		cv::drawContours( mask, cont, 0, cv::Scalar(255, 255, 255), thickness, 8);
+	}
+
+	return mask;
+}
+
 cv::Mat get3x3FromVector(boost::array<double, 9> vec)
 {
 	cv::Mat mat = cv::Mat(3, 3, CV_32F);
@@ -221,7 +245,7 @@ cv::Mat get3x3FromVector(boost::array<double, 9> vec)
 void cameraCallback0(const sensor_msgs::ImageConstPtr& img, const sensor_msgs::CameraInfoConstPtr& cam)
 {
 
-	ROS_DEBUG("reading message");
+	//ROS_DEBUG("reading message");
 	cv::Mat temp = cv_bridge::toCvShare(img, "bgr8")->image.clone();
 
 
@@ -261,17 +285,43 @@ void run(Frame* f)
 		if(approx.size() == 4)
 		{
 			f->quads.push_back(Quadrilateral(approx));
-			f->quads.back().color = getQuadColor(f, f->quads.back());
 		}
 	}
 
 	removeDuplicateQuads(f->quads);
 
+	cv::Mat mask, measurement;
+	mask = createGridMask(f->quads, f->img.rows, f->img.cols);
+	f->img.copyTo(measurement, mask); // create the data to be used
+
+	//cv::GaussianBlur(measurement, measurement, cv::Size(21, 21), 21, 21);
+	cv::GaussianBlur(measurement, measurement, cv::Size(0, 0), 5, 5);
+
+	//cv::fisheye::undistortImage(measurement, measurement, f->K, f->D, cv::Mat::eye(3, 3, CV_32F));
+	cv::fisheye::undistortImage(measurement, measurement, f->K, f->D, f->K);
+
 
 #ifdef SUPER_DEBUG
-	cv::Mat final = f->img;
+	final = measurement;
+	/*
+	cv::Mat temp;
+	f->img.copyTo(temp);
+	final = cv::Mat::zeros(temp.rows, temp.cols, CV_8U);
+	temp.copyTo(final, createGridMask(f->quads, temp.rows, temp.cols));*/
+	//temp.copyTo(final);
+	//final = createGridMask(f->quads, temp.rows, temp.cols);
 
+	/*final = cv::Mat::zeros(final.rows, final.cols, final.type());
 
+	for(auto t : f->quads)
+	{
+		for(auto e : t.test_points)
+		{
+			cv::drawMarker(final, e, temp.at<cv::Vec3b>(e));
+		}
+	}*/
+
+	/*
 	cv::RNG rng(12345);
 	for( int i = 0; i< f->quads.size(); i++ )
 	{
@@ -280,31 +330,27 @@ void run(Frame* f)
 		cont.push_back(f->quads.at(i).contour);
 		cv::drawContours( final, cont, 0, color, 2, 8);
 		switch (f->quads.at(i).color) {
-			case Color::WHITE:
-				ROS_DEBUG_STREAM("drawing white marker");
-				cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(255, 255, 255));
-				break;
-			case Color::GREEN:
-				ROS_DEBUG_STREAM("drawing green marker");
-				cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(0, 255, 0));
-				break;
-			case Color::RED:
-				ROS_DEBUG_STREAM("drawing red marker");
-				cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(0, 0, 255));
-				break;
-			default:
-				ROS_DEBUG_STREAM("drawing other marker");
-				cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(255, 0, 255));
-				break;
+		case Color::WHITE:
+			ROS_DEBUG_STREAM("drawing white marker");
+			//cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(255, 255, 255));
+			break;
+		case Color::GREEN:
+			ROS_DEBUG_STREAM("drawing green marker");
+			//cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(0, 255, 0));
+			break;
+		case Color::RED:
+			ROS_DEBUG_STREAM("drawing red marker");
+			cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(0, 0, 255));
+			break;
+		default:
+			ROS_DEBUG_STREAM("drawing other marker");
+			//cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(255, 0, 255));
+			break;
 		}
-		cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(255, 255, 255), cv::MarkerTypes::MARKER_STAR);
+		//cv::drawMarker(final, f->quads.at(i).center, cv::Scalar(255, 255, 255), cv::MarkerTypes::MARKER_STAR);
+	}*/
 
-		for(auto e : f->quads.at(i).test_points)
-		{
-			cv::drawMarker(final, e, cv::Scalar(255, 0, 255));
-		}
-	}
-
+	imgReady = true;
 	cv::imshow("debug", final);
 	cv::waitKey(30);
 #endif
@@ -346,12 +392,82 @@ int main(int argc, char **argv)
 
 	while(n.ok()){
 
-		ROS_DEBUG("spinning once");
+		//ROS_DEBUG("spinning once");
 		ros::spinOnce();
-		ROS_DEBUG("spun once");
+		//ROS_DEBUG("spun once");
 		loop_rate.sleep();
 
+#ifdef SUPER_DEBUG
+		if(imgReady){
+		cv::imshow("debug", final);
+		cv::waitKey(30);}
+#endif
 	}
 
 	return 0;
 }
+
+/*
+ *
+ * get the color of a pixel by searching around it for colors in the THRESH
+
+Color getCornerColor(Frame* f, cv::Point& pos)
+{
+	Color theColor = Color::OTHER;
+
+
+	cv::Rect roi(pos.x, pos.y, 1, 1);
+	cv::Mat sample;
+	f->img(roi).copyTo(sample);
+	cv::Mat hsv;
+	cv::cvtColor(sample, hsv, CV_BGR2HSV);
+
+	ROS_DEBUG_STREAM("hsv: " << hsv);
+
+
+	//red
+	if(checkPixel(hsv, RED_MIN1, RED_MAX1))
+	{
+		ROS_DEBUG("found red corner");
+		return Color::RED;
+	}
+	if(checkPixel(hsv, RED_MIN2, RED_MAX2))
+	{
+		ROS_DEBUG("found red corner");
+		return Color::RED;
+	}
+
+
+
+
+	return theColor;
+}
+
+
+ * get the color of a quadrilateral
+ * RED = quad near the RED line
+ * GREEN = quad near the GREEN Line
+ * WHITE = quad in the center of the grid
+ * OTHER = outlier
+
+Color getQuadColor(Frame* f, Quadrilateral quad)
+{
+	Color finalColor = Color::WHITE;
+
+	for(std::vector<cv::Point>::iterator it = quad.contour.begin(); it != quad.contour.begin() + 4; it++)
+	{
+		//cv::Point test = (cv::Point2f((*it)) - quad.center) * QUAD_STRECH_FACTOR + quad.center;
+		Color thisCorner = getCornerColor(f, (*it));
+		if(thisCorner == Color::OTHER)
+		{
+			return Color::OTHER; // this is an invalid corner
+		}
+		else if(thisCorner == Color::GREEN || thisCorner == Color::RED)
+		{
+			return thisCorner;
+		}
+	}
+
+	return finalColor;
+}
+ */
